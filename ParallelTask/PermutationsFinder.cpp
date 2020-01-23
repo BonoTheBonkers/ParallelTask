0ada2dce44cfeb1c70c2eb7ab8a8486c3182f796
Kamil Bonecki <kamil.bonecki@realworld-one.com>
2020-01-23 21:06:10 +0100

    Update PermutationsFinder.cpp

--- ParallelTask/PermutationsFinder.cpp
+++ ParallelTask/PermutationsFinder.cpp
@@ -1,18 +1,48 @@
 #include "pch.h"
 #include "PermutationsFinder.h"
+#include <iostream>
+#include <algorithm>
 
+using namespace std;
 
 PermutationsFinder::PermutationsFinder()
 {
 }
 
-
 PermutationsFinder::~PermutationsFinder()
 {
 }
 
-int PermutationsFinder::FindPermutations()
+// Function to display the array 
+void display(int a[], int n)
 {
-	return 0;
+	for (int i = 0; i < n; i++) {
+		cout << a[i] << "  ";
+	}
+	cout << endl;
 }
 
+// Function to find the permutations 
+void findPermutations(int a[], int n)
+{
+
+	// Sort the given array 
+	sort(a, a + n);
+
+	// Find all possible permutations 
+	cout << "Possible permutations are:\n";
+	do {
+		display(a, n);
+	} while (next_permutation(a, a + n));
+}
+
+int PermutationsFinder::FindPermutations()
+{
+	int digits[] = {0,1,2,3,4,5,6,7,8,9};
+
+	int n = sizeof(digits) / sizeof(digits[0]);
+
+	findPermutations(digits, n);
+
+	return 0;
+}
